7daa014127aa490a9697a46dd99372b59d2c4f9f
saqlainhaider27 <138958018+saqlainhaider27@users.noreply.github.com>
2025-04-24 12:21:55 +0500

    update

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
     email: 'saqlainhaider2700@gmail.com',
-    phone: '',
-    address: '',
+    phone: '+92 327 8896738',
+    address: 'Lahore, Pakistan',
     github: 'https://github.com/',
-    facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/',
-    twitter: 'https://twitter.com/',
-    stackOverflow: 'https://stackoverflow.com/',
+    facebook: 'https://github.com/saqlainhaider27\'',
+    linkedIn: 'https://www.linkedin.com/in/saqlain-haider-486b19348/',
+    twitter: 'https://x.com/SaqlainDev27',
+    stackOverflow: 'https://stackoverflow.com/users/29581188/saqlain-haider',
     devUsername: ""
 }
